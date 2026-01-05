af1ef29bd75fe1922b8e32ef774785c4e9128978
Andie <andrea.c.siervo@gmail.com>
2026-01-05 23:14:49 +0000

    First commit

---
+++ src/js/components/Welcome.jsx
@@ -0,0 +1,15 @@
+const Welcome = () => {
+  return (
+    <div className="welcome">
+      <h1 className="welcome-title">A Warm Welcome!</h1>
+
+      <p className="welcome-text">
+       Potter no era un apellido tan especial. Estaba seguro de que había muchísimas personas que se llamaban Potter y que tenían un hijo llamado Harry. Y pensándolo mejor, ni siquiera estaba seguro de que su sobrino se llamara Harry. 
+      </p>
+
+      <button className="welcome-button">Harry Potter</button>
+    </div>
+  );
+};
+
+export default Welcome;
---
+++ src/js/components/cards.jsx
@@ -0,0 +1,45 @@
+<div class="cards">
+  <div class="card">
+    <div class="card-top"></div>
+    <div class="card-body">
+      <h3>Card title</h3>
+      <p>como estas</p>
+    </div>
+    <div class="card-footer">
+      <button>Find Out More!</button>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-top"></div>
+    <div class="card-body">
+      <h3>Card title</h3>
+      <p>como estas</p>
+    </div>
+    <div class="card-footer">
+      <button>Find Out More!</button>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-top"></div>
+    <div class="card-body">
+      <h3>Card title</h3>
+      <p>como estas</p>
+    </div>
+    <div class="card-footer">
+      <button>Find Out More!</button>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-top"></div>
+    <div class="card-body">
+      <h3>Card title</h3>
+      <p>como estas</p>
+    </div>
+    <div class="card-footer">
+      <button>Find Out More!</button>
+    </div>
+  </div>
+</div>
